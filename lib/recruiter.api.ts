c5d681d52c5233df981a3be5d39319edceb02602
UdhayKrishn <krishnauday320@gmail.com>
2025-12-04 11:18:34 +0530

    feat(api): add recruiter-specific api functions

---
+++ lib/recruiter.api.ts
@@ -0,0 +1,6 @@
+import { createApiInstance } from "./axios";
+
+export const recruiterApi = createApiInstance({
+    refreshUrl: "/auth/recruiter/refresh",
+    loginRedirect: "/recruiter/auth/login",
+});
