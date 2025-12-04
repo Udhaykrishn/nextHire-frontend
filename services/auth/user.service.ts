e1f04ff9d0d5b57e436b827b043acece51da6cf5
UdhayKrishn <krishnauday320@gmail.com>
2025-12-04 11:18:35 +0530

    feat(services): add user-specific auth service

---
+++ services/auth/user.service.ts
@@ -0,0 +1,37 @@
+import { LoginFormData } from "@/modules/auth/types";
+import { userApi } from "@/lib/user.api";
+import { API_ROUTES } from "@/constants/api-routes";
+import { SignupPayload } from "@/modules/auth/hooks/useSignup";
+
+export interface AuthResponse {
+    accessToken: string;
+    sessionId: string;
+    user: any;
+}
+export const UserAuthService = {
+    login: async (data: Omit<LoginFormData, 'role'>): Promise<AuthResponse> => {
+        console.log("user auth service is working: ", data)
+        const response = await userApi.post<AuthResponse>(API_ROUTES.AUTH.USER.LOGIN, data);
+        console.log("user login backend res is: ", response.data)
+        return response.data;
+    },
+
+    signup: async (data: Omit<SignupPayload, 'role'>): Promise<AuthResponse> => {
+        const response = await userApi.post<AuthResponse>(API_ROUTES.AUTH.USER.SIGNUP, data);
+        return response.data;
+    },
+
+    logout: async (): Promise<void> => {
+        await userApi.post(API_ROUTES.AUTH.USER.LOGOUT);
+    },
+
+    verifyOtp: async (data: { email: string; otp: string }): Promise<AuthResponse> => {
+        const response = await userApi.post<AuthResponse>(API_ROUTES.AUTH.USER.OTP_VERIFY, data);
+        return response.data;
+    },
+
+    resendOtp: async (data: { email: string }): Promise<{ otp: string }> => {
+        const response = await userApi.post<{ otp: string }>(API_ROUTES.AUTH.USER.RESEND_OTP, data);
+        return response.data;
+    }
+};
