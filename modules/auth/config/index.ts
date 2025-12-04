8778bdf03a7a0af6c711574525e8931a612c2d34
UdhayKrishn <krishnauday320@gmail.com>
2025-12-04 11:18:34 +0530

    feat(auth): add auth module configuration

---
+++ modules/auth/config/index.ts
@@ -0,0 +1,16 @@
+import { RoleConfig } from "../types";
+
+export const roleConfig: Record<string, RoleConfig> = {
+    admin: {
+        showSocialSignup: false,
+        showForgotPassword: false,
+    },
+    user: {
+        showSocialSignup: true,
+        showForgotPassword: true,
+    },
+    recruiter: {
+        showSocialSignup: false,
+        showForgotPassword: true,
+    },
+};
