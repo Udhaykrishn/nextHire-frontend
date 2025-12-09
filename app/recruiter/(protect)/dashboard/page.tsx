9b6c6d9c287fb1f8acf1c2337f3c37f295a91a24
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:44:33 +0530

    feat(recruiter-dashboard): update dashboard page

--- app/recruiter/(protect)/dashboard/page.tsx
+++ app/recruiter/(protect)/dashboard/page.tsx
@@ -1,11 +1,10 @@
-import { userApi } from '@/lib/user.api';
 import { DashboardClient } from './dashboard-client';
+import { recruiterApi } from '@/lib/recruiter.api';
 
 export default async function Page() {
     try {
-        const { data } = await userApi.get("/user", { params: { page: 1, limit: 10 } });
-        console.log(data);
-        return <DashboardClient usersData={data} />;
+        const { data } = await recruiterApi.get("/recruiter/profile");
+        return <DashboardClient usersData={data.data} />;
     } catch (error: any) {
         if (error?.digest?.startsWith('NEXT_REDIRECT')) {
             throw error;
