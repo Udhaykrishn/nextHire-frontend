bc6a011227a3d03c5f04d7c99541a98e21fb6872
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:44:33 +0530

    feat(user-auth): update signup page

--- app/users/auth/signup/page.tsx
+++ app/users/auth/signup/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 
 import SignupPage from "@/modules/auth/pages/signup"
-import { AppLayout } from "@/components/app-layout"
 
 
 const page = () => {
     return (
-        <AppLayout>
-            <SignupPage role="user" />
-        </AppLayout>
+        <SignupPage role="user" />
     )
 }
 
