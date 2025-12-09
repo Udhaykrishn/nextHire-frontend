ddb60f216a808932fdb25422cca6d41da4c776ea
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:44:33 +0530

    feat(recruiter-auth): update otp verify page

--- app/recruiter/auth/otp-verify/page.tsx
+++ app/recruiter/auth/otp-verify/page.tsx
@@ -1,5 +1,6 @@
+import { USER_ROLES } from "@/constants";
 import OtpVerifyPage from "@/modules/auth/pages/otp-verify";
 
 export default function RecruiterOtpVerifyPage() {
-    return <OtpVerifyPage />;
+    return <OtpVerifyPage role={USER_ROLES.RECRUITER} />;
 }
