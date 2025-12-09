f58b0c3bd17a553c40fa5ced6a2ae48f1e1d3ef2
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:44:33 +0530

    feat(home): update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@
 
 import { SlidingNumber } from "@/components/animate-ui/primitives/texts/sliding-number";
 import { AnimatedThemeToggle } from "@/components/animated-theme-toggle";
-import { AppLayout } from "@/components/app-layout";
 
 export default function Home() {
   return (
-    <AppLayout>
-      <div className="flex min-h-screen items-center justify-center gap-4 bg-zinc-50 font-sans dark:bg-zinc-900">
-        <SlidingNumber number={123} animate={true} />
+    <div className="flex min-h-screen items-center justify-center gap-4 bg-zinc-50 font-sans dark:bg-zinc-900">
+      <SlidingNumber number={123} animate={true} />
 
-        <AnimatedThemeToggle direction="ltr" />
-      </div>
-    </AppLayout>
+      <AnimatedThemeToggle direction="ltr" />
+    </div>
   );
 }
