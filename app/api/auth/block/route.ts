c738ad569ff4d4255ef17516f62bebb22395451d
UdhayKrishn <krishnauday320@gmail.com>
2025-12-16 17:09:17 +0530

    feat: block api created

--- app/api/auth/block/route.ts
+++ app/api/auth/block/route.ts
@@ -1,4 +1,3 @@
-
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
@@ -7,25 +6,25 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET(request: Request) {
-    const cookieHeader = request.headers.get("cookie") || "";
-    const { searchParams } = new URL(request.url);
-    const role = searchParams.get("role");
-    const backendUrl = process.env.NEXT_PUBLIC_API_URL;
+  const cookieHeader = request.headers.get("cookie") || "";
+  const { searchParams } = new URL(request.url);
+  const role = searchParams.get("role");
+  const backendUrl = process.env.NEXT_PUBLIC_API_URL;
 
-    const blockResponse = await fetch(`${backendUrl}/${role}/profile`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            cookie: cookieHeader,
-        },
-        credentials: "include",
-    });
+  const blockResponse = await fetch(`${backendUrl}/${role}/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      cookie: cookieHeader,
+    },
+    credentials: "include",
+  });
 
-    const blockData = await blockResponse.json();
+  const blockData = await blockResponse.json();
 
-    if (blockData.error.statusCode === 403) {
-        return NextResponse.json({ blocked: true });
-    }
+  if (blockData.error.statusCode === 403) {
+    return NextResponse.json({ blocked: true });
+  }
 
-    return NextResponse.json({ blocked: false });
+  return NextResponse.json({ blocked: false });
 }
--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,4 +1,3 @@
-
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
@@ -7,16 +6,15 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET(request: Request) {
-    const cookieHeader = request.headers.get("cookie");
-    const accessToken = cookieHeader?.match(/accessToken=([^;]+)/)?.[1];
-    const sessionId = cookieHeader?.match(/session_id=([^;]+)/)?.[1];
+  const cookieHeader = request.headers.get("cookie");
+  const accessToken = cookieHeader?.match(/accessToken=([^;]+)/)?.[1];
+  const sessionId = cookieHeader?.match(/session_id=([^;]+)/)?.[1];
 
-    console.log("access token or refresh token ", accessToken, sessionId)
+  console.log("access token or refresh token ", accessToken, sessionId);
 
+  if (accessToken || sessionId) {
+    return NextResponse.json({ loggedIn: true });
+  }
 
-    if (accessToken || sessionId) {
-        return NextResponse.json({ loggedIn: true });
-    }
-
-    return NextResponse.json({ loggedIn: false });
+  return NextResponse.json({ loggedIn: false });
 }
