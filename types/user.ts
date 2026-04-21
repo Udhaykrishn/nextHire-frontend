cabd0c0cdba0e44fffdc9a74dee2e9178f2d68bc
uday <krishnauday320@gmail.com>
2026-04-21 21:19:42 +0530

    feat(types): add role property to User interface

---
+++ types/user.ts
@@ -0,0 +1,36 @@
+export interface User {
+  role: string;
+  id?: string;
+  email: string;
+  name: string;
+  phone: string;
+  experience: string;
+  role_of_title: string;
+  status: string;
+  resume_url: {
+    key: string;
+    url: string;
+  };
+  profile_url: {
+    key: string;
+    url: string;
+  };
+  bio: string;
+  badge: boolean;
+  google_id: string;
+  subscription: {
+    current_plan: string;
+    is_subscribed: boolean;
+  };
+  social_link: {
+    linkedin: string;
+    portfolio: string;
+    github: string;
+  };
+  skills: string[];
+  languages: {
+    name: string;
+    proficiency: string;
+  }[];
+  createdAt?: string | Date;
+}
